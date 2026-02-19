fa772807909519e8a9d0f2019e4686773e466130
muhammad alfaiq rido salafy <alfaiqrido@gmail.com>
2026-02-19 22:05:16 +0700

    New Titik Merged desc

--- Pertemuan1/src/MTitik.java
+++ Pertemuan1/src/MTitik.java
@@ -1,3 +1,9 @@
+/*  Nama File   : MTitik.java
+    Deskripsi   : Main dalam class Titik
+    Pembuat     : Muhammad Alfaiq Rido Salafy/24060124140148
+    Tanggal     : 19/02/2026
+ */
+
 public class MTitik {
     public static void main(String[] args){
         Titik T1 = new Titik();
